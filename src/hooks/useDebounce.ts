1d4b7e7c99fe5bf9c2186045e698debc74c38534
fridoa <fridoafriyanto3@gmail.com>
2025-09-30 23:59:10 +0700

    feat(hooks): add useDebounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+
+const useDebounce = () => {
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+  return (func: () => void, delay: number) => {
+    if (debounceTimeout.current) {
+      clearTimeout(debounceTimeout.current);
+    }
+    debounceTimeout.current = setTimeout(() => {
+      func();
+      debounceTimeout.current = null;
+    }, delay);
+  };
+};
+
+export default useDebounce;
